bc0b0428d9868488b249e7ac2011684a50d2eb29
Alexander Treimanis <treimanis@Alexanders-Air.cable.rcn.com>
2022-03-09 17:57:54 -0600

    finished all of the articles component

--- src/components/article/article.js
+++ src/components/article/article.js
@@ -87,6 +87,13 @@ const data = [
     thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
           Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
           Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: 'A new article is born',
+    date: 'March 9, 2022',
+    firstParagraph: 'Paragraph number one, blah blah blah blah',
+    secondParagraph: 'Paragraph number two, blah blah blah blah',
+    thirdParagraph: 'Paragraph number three, blah blah blah blah'
   }
 ];
 
